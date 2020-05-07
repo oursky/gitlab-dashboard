ff9d22f910766c22b59828c74a93d32eaecd4cbf
mauricehuang <mauricehuang9@gmail.com>
2020-05-07 19:03:32 +0800

    Create pipeline database model and make gitlab request.

--- backend/external/gitlabAPI.py
+++ backend/external/gitlabAPI.py
@@ -1,11 +1,42 @@
 import requests
-from backend.models import Project
+from backend.models import Project, Pipeline
+MOCK_USER_ID = '5758391'
+
+# TODO: add requests access token header
 
 
-def getProjectIdFromApi():
-    r = requests.get('https://gitlab.com/api/v4/users/5758391/projects/')
-    print(r.json()[0]['id'])
-    newId = r.json()[0]['id']
-    p = Project(projectId=newId)
-    p.save()
-    print(p)
+def executeFetch():
+    fetchProject()
+    fetchPipeline()
+
+
+def fetchProject():
+    url = 'https://gitlab.com/api/v4/users/' + MOCK_USER_ID + '/projects'
+    r = requests.get(url)
+    project_list = r.json()
+    for p in project_list:
+        project = Project(id=p['id'], name=p['name'])
+        project.save()
+
+
+def fetchPipeline():
+    project_list = Project.objects.all()
+    for p in project_list:
+        p_id = p.id
+        project_url = 'https://gitlab.com/api/v4/projects/' + p_id + '/pipelines'
+        project_response = requests.get(project_url)
+        pipeline_list = project_response.json()
+        for p in pipeline_list:
+            pipeline_id = str(p['id'])
+            # TODO: check if record is in DB and is finished, if so do not fetch with request
+            pipeline_url = project_url + '/' + pipeline_id
+            pipeline_response = requests.get(pipeline_url)
+            pipeline_data = pipeline_response.json()
+            commit_id = pipeline_data['sha']
+            commit_url = 'https://gitlab.com/api/v4/projects/' + \
+                p_id + '/repository/commits/' + commit_id
+            commit_response = requests.get(commit_url)
+            commit_data = commit_response.json()
+            pipeline = Pipeline(id=pipeline_data['id'], project_id=p_id, status=pipeline_data['status'], branch_ref=pipeline_data['ref'], commit_id=pipeline_data['sha'], commit_author=pipeline_data['user']
+                                ['name'], commit_message=commit_data['message'], created_at=pipeline_data['created_at'], updated_at=pipeline_data['updated_at'], finished_at=pipeline_data['finished_at'])
+            pipeline.save()
--- backend/models.py
+++ backend/models.py
@@ -4,4 +4,18 @@ from django.db import models
 
 
 class Project(models.Model):
-    projectId = models.CharField(max_length=100)
+    id = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=128)
+
+
+class Pipeline(models.Model):
+    id = models.CharField(max_length=32, primary_key=True)
+    project_id = models.CharField(max_length=32)
+    status = models.CharField(max_length=32)
+    branch_ref = models.CharField(max_length=128)
+    commit_id = models.CharField(max_length=32)
+    commit_author = models.CharField(max_length=32)
+    commit_message = models.CharField(max_length=128)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    finished_at = models.DateTimeField()
