576f66da76af11ae900d609d20f8623b6bae8ab3
mauricehuang <mauricehuang9@gmail.com>
2020-05-19 16:28:06 +0800

    Implement background styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,32 +5,15 @@ import {Project, Pipeline, Runner} from './models/models';
 import QueuingPipelineListContainer from './container/QueuingPipelineContainer';
 import CompletedPipelineListContainer from './container/CompletedPipelineListContainer';
 import RunnerListContainer from './container/RunnerContainer';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import {Theme, createStyles, makeStyles} from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      padding: theme.spacing(4),
-    },
-    paper: {
-      paddingLeft: theme.spacing(4),
-      paddingRight: theme.spacing(4),
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(3),
-    },
-  }),
-);
+import {Container, Grid, Paper} from "@material-ui/core";
+import {useRootStyles} from './styles/styles';
 
 function App() {
   const [projectList, setProjectList] = useState<Project[]>([]);
   const [pipelineList, setPipelineList] = useState<Pipeline[]>([]);
   const [runnerList, setRunnerList] = useState<Runner[]>([]);
 
-  const classes = useStyles();
+  const classes = useRootStyles();
 
   useEffect(() => {
     fetchFromBackend(setProjectList, setPipelineList, setRunnerList);
@@ -41,26 +24,26 @@ function App() {
       <Container className={classes.root} maxWidth="lg">
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <h2>Queuing Projects and Pipeline</h2>
+            <Paper elevation={0} className={classes.paper}>
+              <h3>Queuing Project Pipelines</h3>
               <QueuingPipelineListContainer projects={projectList} pipelines={pipelineList}></QueuingPipelineListContainer>
             </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <h2>Completed Projects and Pipeline</h2>
+            <Paper elevation={0} className={classes.paper}>
+              <h3>Completed Project Pipelines</h3>
               <CompletedPipelineListContainer projects={projectList} pipelines={pipelineList}></CompletedPipelineListContainer>
             </Paper>
           </Grid>
           <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <h2>Runners</h2>
+            <Paper elevation={0} className={classes.paper}>
+              <h3>Runners</h3>
               <RunnerListContainer runners={runnerList}></RunnerListContainer>
             </Paper>
           </Grid>
         </Grid>
       </Container>
-    </div >
+    </ div >
   );
 }
 
---
+++ frontend/src/styles/styles.ts
@@ -0,0 +1,52 @@
+import {makeStyles, Theme, createStyles} from "@material-ui/core";
+import {lightBlue} from "@material-ui/core/colors";
+
+export const useRootStyles = makeStyles((theme: Theme) =>
+createStyles({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(4),
+      backgroundColor: lightBlue[50],
+      minHeight: "100vh",
+    },
+    paper: {
+      paddingLeft: theme.spacing(4),
+      paddingRight: theme.spacing(4),
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(3),
+    },
+  })
+);
+
+export const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+            flexDirection: "column",
+        },
+        card: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            height: 60,
+            fontSize:5,
+            lineHeight: 1.5,
+        },
+        projectNameCard: {
+            fontSize: 12,
+        },
+        pipelineCard: {
+            fontSize: 8,
+        },
+        box: {
+        },
+        content: {
+            padding: 0,
+            margin: 0,
+        },
+        smallContent: {
+            fontSize: 6,
+        }
+    }),
+);
