e7520d46f103fa35a93eb12c5880bc3d1539c54e
mauricehuang <mauricehuang9@gmail.com>
2020-05-20 17:55:01 +0800

    Implement backend fetch from gitlab API periodically

--- backend/apps.py
+++ backend/apps.py
@@ -1,5 +1,28 @@
 from django.apps import AppConfig
+import threading
+import schedule
+import time
+
+GITLAB_API_FETCH_INTERVAL = 30
+
+
+def fetchJob():
+    from backend.external.gitlabAPI import executeFetch
+    print("fetch start")
+    executeFetch()
+    print("fetch completed")
+
+
+def worker():
+    schedule.every(GITLAB_API_FETCH_INTERVAL).seconds.do(fetchJob)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 class BackendConfig(AppConfig):
     name = 'backend'
+
+    def ready(self):
+        t = threading.Thread(target=worker)
+        t.start()
